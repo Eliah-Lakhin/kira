ccf5b2b781a4e54183bf2d87aa89a6f9fcacdc9d
Ilya Lakhin <eliah.lakhin@gmail.com>
2012-07-28 00:42:40 +0700

    Grunt initial configuration was done

--- grunt.js
+++ grunt.js
@@ -1,4 +1,26 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-  });
+    grunt.initConfig({
+        pkg: "<json:package.json>",
+        meta: {
+            banner: grunt.file.read("misc/banner.txt")
+        },
+        concat: {
+            sources: {
+                src: ["<banner>", "<file_strip_banner:src/kira.js>"],
+                dest: "build/<%= pkg.name %>-<%= pkg.version %>.js"
+            }
+        },
+        min: {
+            dist: {
+                src: "build/<%= pkg.name %>-<%= pkg.version %>.js",
+                dest: "build/<%= pkg.name %>-min-<%= pkg.version %>.js"
+            }
+        },
+        watch: {
+            files: ["src/*.js", "test/*.js"],
+            tasks: "concat min"
+        }
+    });
+
+    grunt.registerTask("default", "concat min");
 };
