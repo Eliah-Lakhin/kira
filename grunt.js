97a39b2d660300e9c231a72163eff89ca16684bd
Ilya Lakhin <eliah.lakhin@gmail.com>
2012-07-28 15:09:32 +0700

    JsTestDriver server starting up feature was implemented

--- grunt.js
+++ grunt.js
@@ -19,8 +19,58 @@ module.exports = function(grunt) {
         watch: {
             files: ["src/*.js", "test/*.js"],
             tasks: "concat min"
+        },
+        jstdServer: {
+            port: 9876,
+            config: "test/kira-base-features.jstd",
+            driver: "lib/JsTestDriver.jar"
         }
     });
 
-    grunt.registerTask("default", "concat min");
+    var jstdServerProcess = null;
+
+    grunt.registerTask("jstdServer", "JsTestDriver server starting up", function() {
+        var done = grunt.utils._.debounce(this.async(), 300);
+
+        var exec = require("child_process").exec;
+
+        grunt.config.requires("jstdServer");
+        var port = grunt.config.get("jstdServer").port,
+            config = grunt.config.get("jstdServer").config,
+            driver = grunt.config.get("jstdServer").driver;
+
+        var start = function() {
+            grunt.verbose.writeln("Starting JsTestDriver server on port " + port + "...");
+            var command = "java -jar " + driver + " --config \"" + config + "\" --port " + port;
+            grunt.verbose.writeln("Command to execute: " + command);
+            jstdServerProcess = exec(command, function(error, stdout, stderr) {
+                if (error !== null) {
+                    grunt.log.writeln("JsTestDriver startup error:");
+                    grunt.log.writeln(error);
+                    grunt.log.writeln(stdout);
+                    grunt.log.writeln(stderr);
+                    done(false);
+                }
+            });
+            jstdServerProcess.on("exit", function() {
+                jstdServerProcess = null;
+            });
+            jstdServerProcess.stdout.on("data", function(data) {
+                grunt.log.writeln("JsTestDriver server started:");
+                grunt.log.writeln(data);
+                done(true);
+            });
+        };
+
+        if (jstdServerProcess !== null) {
+            jstdServerProcess.on("exit", function() {
+                start();
+            });
+            jstdServerProcess.kill();
+        } else {
+            start();
+        }
+    });
+
+    grunt.registerTask("default", "jstdServer");
 };
