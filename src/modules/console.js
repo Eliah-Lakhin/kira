f4bc5e639ad233cfa86d13b7125e1926ce16f4de
Ilya Lakhin <eliah.lakhin@gmail.com>
2012-09-18 16:22:55 +0700

    Console module interface was simplified

--- src/modules/console.js
+++ src/modules/console.js
@@ -21,20 +21,14 @@
     var console = context.console;
 
     kira.console = {
-        log: function(message) {
+        watch: function(message) {
             console.log(message);
             return message;
         },
+
         profile: function(block) {
             var start = new Date().getTime();
             block();
             return new Date().getTime() - start;
-        },
-        logProfile: function(message, block) {
-            if (block === undefined) {
-                block = message;
-                message = "%s";
-            }
-            console.log(message.replace("%s", kira.console.profile(block)));
         }
     };
